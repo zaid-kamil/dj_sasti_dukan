77f13119b175834eccba2e20540e5c9faba34c51
zaid kamil <xaidmetamorphos@gmail.com>
2024-09-10 14:40:30 +0530

    views

--- main/views.py
+++ main/views.py
@@ -66,9 +66,11 @@ def seller_login_view(request):
                 else:
                     messages.error(request, 'You are not registered as a seller')
     return render(request, 'accounts/seller/login.html')
+
 def seller_register_view(request):
 
     return render(request, 'accounts/seller/register.html')
+
 def seller_forgot_pass_view(request):
     
     return render(request, 'accounts/seller/forgot_password.html')
@@ -76,7 +78,9 @@ def seller_forgot_pass_view(request):
 # customer views
 def customer_login_view(request):
     return render(request, 'accounts/customer/login.html')
+
 def customer_register_view(request):
     return render(request, 'accounts/customer/register.html')
+
 def customer_forgot_pass_view(request):
     return render(request, 'accounts/customer/forgot_password.html')
