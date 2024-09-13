1d1c3ec77c9238fd797dd8f8f63ebbcd51c8fe80
zaid kamil <xaidmetamorphos@gmail.com>
2024-09-13 14:09:56 +0530

    added less that 50 lines of code

--- main/views.py
+++ main/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect, get_object_or_404, get_list_or_40
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.models import User, Group
 from django.contrib import messages
-from product.models import Product, Category
+from product.models import Product, Category, Review
+from product.forms import ReviewForm
 
 def logout_view(request):
     logout(request)
@@ -33,9 +34,13 @@ def detail_view(request, id):
     product = Product.objects.get(id=id)
     # get upto 3 similar products
     similar_products = Product.objects.filter(category=product.category).exclude(id=id).order_by('?')[:3] # order by random
+    # get latest reviews
+    reviews = Review.objects.filter(product=product).order_by('-created_at') # reverse order
     ctx = {
         'product': product,
-        'similar_products': similar_products
+        'similar_products': similar_products,
+        'reviews': reviews,
+        'review_form': ReviewForm(),
     }
     return render(request, 'detail.html', ctx)
 
